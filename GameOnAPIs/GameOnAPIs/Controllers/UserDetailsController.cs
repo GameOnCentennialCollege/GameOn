627828cb348b7b84e76fe2e8a274947fc9b9f60f
Anchal Sharma <aanchal_sharma26@hotmail.com>
2016-10-21 12:48:23 -0400

    Get user details by user id
    User api created to get user details by user id

---
+++ GameOnAPIs/GameOnAPIs/Controllers/UserDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GameOnAPIs;
+
+namespace GameOnAPIs.Controllers
+{
+    public class UserDetailsController : ApiController
+    {
+        private enigma_gameonEntities db = new enigma_gameonEntities();
+
+        // GET: api/UserDetails
+        public IQueryable<UserDetail> GetUserDetails()
+        {
+            return db.UserDetails;
+        }
+
+        // GET: api/UserDetails/5
+        [ResponseType(typeof(UserDetail))]
+        public dynamic GetUserDetail(string user_id)
+        {
+            return new { user = db.sp_user_get_by_id(user_id) };            
+        }
+
+        // PUT: api/UserDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUserDetail(string id, UserDetail userDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userDetail.UserID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(userDetail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/UserDetails
+        [ResponseType(typeof(UserDetail))]
+        public IHttpActionResult PostUserDetail(UserDetail userDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.UserDetails.Add(userDetail);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (UserDetailExists(userDetail.UserID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = userDetail.UserID }, userDetail);
+        }
+
+        // DELETE: api/UserDetails/5
+        [ResponseType(typeof(UserDetail))]
+        public IHttpActionResult DeleteUserDetail(string id)
+        {
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.UserDetails.Remove(userDetail);
+            db.SaveChanges();
+
+            return Ok(userDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserDetailExists(string id)
+        {
+            return db.UserDetails.Count(e => e.UserID == id) > 0;
+        }
+    }
+}
