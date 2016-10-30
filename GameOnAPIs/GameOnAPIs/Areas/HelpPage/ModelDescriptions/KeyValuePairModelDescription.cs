c4480cd15407897445c926f33be4e21d1fa1a6db
Anchal Sharma <aanchal_sharma26@hotmail.com>
2016-10-30 14:39:46 -0400

    Initial Project Created
    Initial project created. ADO data model added.

---
+++ GameOnAPIs/GameOnAPIs/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace GameOnAPIs.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
