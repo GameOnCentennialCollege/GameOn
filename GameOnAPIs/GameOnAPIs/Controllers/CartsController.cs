45bb8547a9d52c6235492276842cf515ad80f955
Anchal Sharma <aanchal_sharma26@hotmail.com>
2016-10-31 15:25:54 -0400

    Cart API created
    insert new item to the cart
    gameon.enigmacondos.ca/api/Carts/PostCart
    POST method
    values to be passed...
    cartstockid, cartquantity, cartunitcost, cartclientid
    example...
    cartstockid: 232, cartquantity: 1, cartunitcost: 55, cartclientid:
    anchal

---
+++ GameOnAPIs/GameOnAPIs/Controllers/CartsController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace GameOnAPIs.Controllers
+{
+    public class CartsController : ApiController
+    {
+        private enigma_gameonEntities db = new enigma_gameonEntities();
+
+        
+
+        // POST: api/Carts
+        /// <summary>
+        /// insert new item to the cart (stock id, quantity, unit cost, user id).
+        /// </summary>
+        [ResponseType(typeof(Cart))]
+        public dynamic PostCart(Cart cart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Carts.Add(cart);
+            db.SaveChanges();
+
+            return new { cart = db.sp_get_cart_details_by_user_id(cart.CartClientID) };
+        }
+
+        //// DELETE: api/Carts/5
+        //[ResponseType(typeof(Cart))]
+        //public IHttpActionResult DeleteCart(int id)
+        //{
+        //    Cart cart = db.Carts.Find(id);
+        //    if (cart == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Carts.Remove(cart);
+        //    db.SaveChanges();
+
+        //    return Ok(cart);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CartExists(int id)
+        {
+            return db.Carts.Count(e => e.CartID == id) > 0;
+        }
+    }
+}
