1bedc1b46923cb8c30216bc02cf86215dd2a6f9f
Anchal Sharma <aanchal_sharma26@hotmail.com>
2016-10-31 00:10:22 -0400

    Color, and Size APIs created
    http://gameon.enigmacondos.ca/api/Sizes/product/{id}
    Get the available sizes for a particular product
    
    http://gameon.enigmacondos.ca/api/Colors/product/{id}
    Get the available colors for a particular product

---
+++ GameOnAPIs/GameOnAPIs/Controllers/ColorsController.cs
@@ -0,0 +1,134 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace GameOnAPIs.Controllers
+{
+    public class ColorsController : ApiController
+    {
+        private enigma_gameonEntities db = new enigma_gameonEntities();
+
+        //// GET: api/Colors
+        //public IQueryable<Color> GetColors()
+        //{
+        //    return db.Colors;
+        //}
+
+        //// GET: api/Colors/5
+        //[ResponseType(typeof(Color))]
+        //public IHttpActionResult GetColor(int id)
+        //{
+        //    Color color = db.Colors.Find(id);
+        //    if (color == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(color);
+        //}
+
+        // GET: api/Colors/product/5
+        [ResponseType(typeof(Color))]
+        [ActionName("product")]
+        public dynamic GetColorsByProductID(int id)
+        {
+
+            return new { color = db.sp_get_colours_by_product_id(id) };
+        }
+
+        //// PUT: api/Colors/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutColor(int id, Color color)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != color.ColorID)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(color).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ColorExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //// POST: api/Colors
+        //[ResponseType(typeof(Color))]
+        //public IHttpActionResult PostColor(Color color)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Colors.Add(color);
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateException)
+        //    {
+        //        if (ColorExists(color.ColorID))
+        //        {
+        //            return Conflict();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return CreatedAtRoute("DefaultApi", new { id = color.ColorID }, color);
+        //}
+
+        //// DELETE: api/Colors/5
+        //[ResponseType(typeof(Color))]
+        //public IHttpActionResult DeleteColor(int id)
+        //{
+        //    Color color = db.Colors.Find(id);
+        //    if (color == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Colors.Remove(color);
+        //    db.SaveChanges();
+
+        //    return Ok(color);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ColorExists(int id)
+        {
+            return db.Colors.Count(e => e.ColorID == id) > 0;
+        }
+    }
+}
---
+++ GameOnAPIs/GameOnAPIs/Controllers/SizesController.cs
@@ -0,0 +1,134 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace GameOnAPIs.Controllers
+{
+    public class SizesController : ApiController
+    {
+        private enigma_gameonEntities db = new enigma_gameonEntities();
+
+        //// GET: api/Sizes
+        //public IQueryable<Size> GetSizes()
+        //{
+        //    return db.Sizes;
+        //}
+
+        //// GET: api/Sizes/5
+        //[ResponseType(typeof(Size))]
+        //public IHttpActionResult GetSize(int id)
+        //{
+        //    Size size = db.Sizes.Find(id);
+        //    if (size == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(size);
+        //}
+
+        // GET: api/Sizes/product/5
+        [ResponseType(typeof(Size))]
+        [ActionName("product")]
+        public dynamic GetSizesByProductID(int id)
+        {
+
+            return new { size = db.sp_get_sizes_by_product_id(id) };
+        }
+
+        //// PUT: api/Sizes/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutSize(int id, Size size)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != size.SizeID)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(size).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!SizeExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //// POST: api/Sizes
+        //[ResponseType(typeof(Size))]
+        //public IHttpActionResult PostSize(Size size)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Sizes.Add(size);
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateException)
+        //    {
+        //        if (SizeExists(size.SizeID))
+        //        {
+        //            return Conflict();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return CreatedAtRoute("DefaultApi", new { id = size.SizeID }, size);
+        //}
+
+        //// DELETE: api/Sizes/5
+        //[ResponseType(typeof(Size))]
+        //public IHttpActionResult DeleteSize(int id)
+        //{
+        //    Size size = db.Sizes.Find(id);
+        //    if (size == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Sizes.Remove(size);
+        //    db.SaveChanges();
+
+        //    return Ok(size);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SizeExists(int id)
+        {
+            return db.Sizes.Count(e => e.SizeID == id) > 0;
+        }
+    }
+}
